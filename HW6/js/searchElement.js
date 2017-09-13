583a42a0b2517e31547699531916aa046e534229
unknown <naga09sindhura@gmail.com>
2017-09-14 01:16:57 +0530

    applied single var pattern in searchElement funtion

--- HW6/js/searchElement.js
+++ HW6/js/searchElement.js
@@ -1,22 +1,22 @@
 ﻿var searchElement = (function () {
     function searchElement() { };
     searchElement.prototype.addSearchfunctionality = function () {
-        var ySearchElementDv = document.createElement("div");
+        var ySearchElementDv = document.createElement("div"),
+            title = document.createElement("h2"),
+            ySearchInputTxb = document.createElement("input"),
+            ySearchBtn = document.createElement("input"),
+            hrElement = document.createElement("hr");
+            
         ySearchElementDv.classList.add('div-search');
 
-        var title = document.createElement("h2");
         title.appendChild(document.createTextNode("Youtube Search Application"));
 
-        var ySearchInputTxb = document.createElement("input");
         ySearchInputTxb.setAttribute("id", "ySearchInputTxb");
         ySearchInputTxb.setAttribute("type", "text");
 
-        var ySearchBtn = document.createElement("input");
         ySearchBtn.setAttribute("type", "button");
         ySearchBtn.setAttribute("value", "search");
         ySearchBtn.addEventListener("click", this.mapSearchInputResults);
-
-        var hrElement = document.createElement("hr");
 
         ySearchElementDv.appendChild(title);
         ySearchElementDv.appendChild(ySearchInputTxb);
@@ -29,7 +29,6 @@
     searchElement.prototype.mapSearchInputResults = function () {
         var searchText = document.querySelector("#ySearchInputTxb");
 
-        alert(searchText.value);
         fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCTWC75i70moJLzyNh3tt4jzCljZcRkU8Y&type=video&part=snippet&maxResults=15&q=' + searchText.value)
                .then(function (response) {
 
